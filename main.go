1c9706325fb17b008f922f51ef45bfdd0c6b38ee
Rizky Darmawan <darmawanrizky43@gmail.com>
2022-12-05 21:35:08 +0700

    features: added another key and function greetings

--- main.go
+++ main.go
@@ -6,10 +6,22 @@ import (
 )
 
 func doSomething(ctx context.Context) {
+	// Create new value
+	greetContext := context.WithValue(ctx, "myKey", "Hola")
+	// Call function greetings and sent greetContext
+	greetings(greetContext)
+
 	// Get value from context
 	val := ctx.Value("myKey")
 	// Print value
 	fmt.Println("Value in context says:", val)
+}
+
+func greetings(ctx context.Context) {
+	// Get value from context
+	val := ctx.Value("myKey")
+	// Print value
+	fmt.Println("Value context in function greetings says:", val)
 }
 
 func main() {
