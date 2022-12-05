98023e051d9d8fdadf9b257bd77d86ab3a4c667e
Rizky Darmawan <darmawanrizky43@gmail.com>
2022-12-05 14:16:13 +0700

    features: use context.WithValue

--- main.go
+++ main.go
@@ -6,10 +6,17 @@ import (
 )
 
 func doSomething(ctx context.Context) {
-	fmt.Println("Hello from context.")
+	// Get value from context
+	val := ctx.Value("myKey")
+	// Print value
+	fmt.Println("Value in context says:", val)
 }
 
 func main() {
+	// Create new context
 	ctx := context.Background()
+	// Add value to context
+	ctx = context.WithValue(ctx, "myKey", "Hello")
+	// Call function doSomething and sent context
 	doSomething(ctx)
 }
