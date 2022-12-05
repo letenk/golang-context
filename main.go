634b51c5055f43d165817026f6233b86e670b6bd
Rizky Darmawan <darmawanrizky43@gmail.com>
2022-12-05 11:18:50 +0700

    features: create context with context.TODO

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+func doSomething(ctx context.Context) {
+	fmt.Println("Hello from context.")
+}
+
+func main() {
+	ctx := context.TODO()
+	doSomething(ctx)
+}
